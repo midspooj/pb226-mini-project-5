67fba383208b4719ff3d1f3317d104d89865a4f6
midspooj <142264378+midspooj@users.noreply.github.com>
2023-10-05 18:08:53 -0400

    Update query.py updated

--- mylib/query.py
+++ mylib/query.py
@@ -7,10 +7,13 @@ def query():
     """Query the database for the top 5 rows of the GroceryDB table"""
     conn = sqlite3.connect("GroceryDB.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM GroceryDB")
-    print("Top 5 rows of the GroceryDB table:")
-    print(cursor.fetchall())
+    cursor.execute("SELECT avg (count_products) FROM GroceryDB")
+    print("Sum of count_products:", cursor.fetchall())
+    cursor.execute("SELECT sum (count_products) FROM GroceryDB")
+    print("Avg of count_products:", cursor.fetchall())
     conn.close()
     return "Success"
 
+
+print(query())
 
